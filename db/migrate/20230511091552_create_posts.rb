2e39fd07a3586b5cdbdfc38f2f28cd69aa7757f9
hanzo6ix <125126141+hanzo6ix@users.noreply.github.com>
2023-05-17 21:12:03 +0000

    5/18

--- db/migrate/20230511091552_create_posts.rb
+++ db/migrate/20230511091552_create_posts.rb
@@ -4,9 +4,10 @@ class CreatePosts < ActiveRecord::Migration[6.1]
 
       t.timestamps
 
+      t.integer :user_id, null: false
       t.integer :genre_id, null: false
       t.integer :feeling_id, null: false
-      t.time :strat_time
+      t.time :start_time
       t.time :finish_time
       t.text :oneword
       t.integer :status , null: false, default: 0
