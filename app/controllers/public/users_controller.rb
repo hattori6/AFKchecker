9fd77b29cc2a16e903c39b3ce0dda4bf8e50e11f
hanzo6ix <125126141+hanzo6ix@users.noreply.github.com>
2023-05-21 06:23:28 +0000

    5/21

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -3,8 +3,15 @@ class Public::UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction)
   end
 end
