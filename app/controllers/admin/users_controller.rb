007453eab2ea2f5c19991a9fd903e6fe07b65922
hanzo6ix <125126141+hanzo6ix@users.noreply.github.com>
2023-06-14 05:55:07 +0000

    fix

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,15 +1,16 @@
 class Admin::UsersController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def index
     @users = User.page(params[:page])
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
   end
-  
+
 
 end
